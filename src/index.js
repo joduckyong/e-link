1639143c6358a97c798c55d6c7f98f265a186a0e
joduckyong <dyddl7609@gmail.com>
2022-12-29 17:14:13 +0900

    로그인 개발중

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
+import { CookiesProvider } from 'react-cookie';
 import store from './store/index';
 import './styles/layout.css';
 import './styles/layout_1440.css';
@@ -11,9 +12,11 @@ import './styles/paging.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <CookiesProvider>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </CookiesProvider>,
 );
 
 reportWebVitals();
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,10 @@
 import { combineReducers, configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import tokenReducer from './Auth';
 import { popupReducer } from './popupReducer';
 import { boardReducer } from './boardReducer';
 import logger from 'redux-logger';
 
-const reducer = combineReducers({ popupReducer: popupReducer.reducer, boardReducer: boardReducer.reducer });
+const reducer = combineReducers({ authToken: tokenReducer, popupReducer: popupReducer.reducer, boardReducer: boardReducer.reducer });
 
 export default configureStore({
   reducer,
