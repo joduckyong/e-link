978b169faa8016d848faa87c0d9cadba9dfc47c1
joduckyong <dyddl7609@gmail.com>
2022-12-29 22:06:42 +0900

    아이디 저장

--- src/components/admin/LoginForm.js
+++ src/components/admin/LoginForm.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { useForm } from 'react-hook-form';
@@ -8,10 +9,32 @@ import { loginUser } from '../../api/Users';
 import { setRefreshToken } from '../../storage/Cookie';
 import { SET_TOKEN } from '../../store/Auth';
 
+import { useCookies } from 'react-cookie';
+
 // import tw from 'twin.macro';
 const LoginForm = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+
+  const [loginId, setLoginId] = useState('');
+  const [isRemember, setIsRemember] = useState(false);
+  const [cookies, setCookie, removeCookie] = useCookies(['rememberId']);
+
+  useEffect(() => {
+    if (cookies.rememberId !== undefined) {
+      setLoginId(cookies.rememberId);
+      setIsRemember(true);
+    }
+  }, [cookies.rememberId]);
+
+  const handleOnChange = (e) => {
+    setIsRemember(e.target.check);
+    if (e.target.check) {
+      setCookie('rememberId', loginId, { maxAge: 2000 });
+    } else {
+      removeCookie('rememberId');
+    }
+  };
 
   // useForm 사용을 위한 선언
   const {
@@ -31,7 +54,9 @@ const LoginForm = () => {
       setRefreshToken(response.json.refreshToken);
       dispatch(SET_TOKEN(response.json.accessToken));
       return navigate('/admin/main/popup');
+      // document.location.href = '/admin/main/popup';
     } else {
+      alert('아이디 비밀번호를 확인해주세요!');
       console.log(response.json);
     }
     // input 태그 값 비워주는 코드
@@ -51,21 +76,21 @@ const LoginForm = () => {
               <input type="hidden" name="remember" defaultValue="true" />
               <div className="login-area">
                 <div className="in">
-                  <input type="text" name="adminId" placeholder="아이디" {...register('adminId', { required: 'Please Enter Your ID' })} />
+                  <input type="text" name="adminId" placeholder="아이디" {...register('adminId', { required: '아이디를 입력해주세요' })} />
                   <ErrorMessage
                     name="adminId"
                     errors={errors}
                     render={({ message }) => <p className="text-sm font-medium text-rose-500">{message}</p>}
                   />
-                  <input type="password" name="adminPw" placeholder="비밀번호" {...register('adminPw', { required: 'Please Enter Your Password' })} />
+                  <input type="password" name="adminPw" placeholder="비밀번호" {...register('adminPw', { required: '비밀번호를 입력해주세요' })} />
                   <ErrorMessage
-                    name="adminId"
+                    name="adminPw"
                     errors={errors}
                     render={({ message }) => <p className="text-sm font-medium text-rose-500">{message}</p>}
                   />
                   <div className="keep-chk">
                     <label htmfor="keep">
-                      <input type="checkbox" id="keep" />
+                      <input type="checkbox" id="keep" onChange={handleOnChange} checked={isRemember} />
                       <span className="chkimg"></span>
                       아이디저장
                     </label>
