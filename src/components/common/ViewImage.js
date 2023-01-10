b8d8770a3506fd8a95cc250240e5f05f81b66962
Kang <khighbig@gmail.com>
2023-01-10 17:53:04 +0900

    수정

--- src/components/common/ViewImage.js
+++ src/components/common/ViewImage.js
@@ -37,7 +37,7 @@ const ViewImage = ({ fileNm, basicStyle=false }) => {
     }
   }, [fileNm, url]);
 
-  return fileNm !== null && <img src={objectUrl} style={!basicStyle ? style : {}} alt="" />;
+  return fileNm !== null && <img src={objectUrl} style={basicStyle ? {} : style} alt="" />;
 };
 
 export default ViewImage;
