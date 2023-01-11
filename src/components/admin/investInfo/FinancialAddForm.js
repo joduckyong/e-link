daf90d357b104dd53995e495aa76be4a90c99770
joduckyong <dyddl7609@gmail.com>
2023-01-11 22:56:41 +0900

    개발중

--- src/components/admin/investInfo/FinancialAddForm.js
+++ src/components/admin/investInfo/FinancialAddForm.js
@@ -1,7 +1,10 @@
 import React, { useState, useRef, useCallback } from 'react';
+import axios from 'axios';
 import { NavLink, useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { insertBoard } from 'store/boardReducer';
+import { serverUrl } from 'store/serverUrl';
+import { getCookieToken } from 'storage/Cookie';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
@@ -26,6 +29,7 @@ const FinancialAddForm = () => {
   const [boardContents, setBoardContents] = useState('');
   const [fileName, setFileName] = useState({});
   const [fileCountList, setFileCountList] = useState([0]);
+  const [image, setImage] = useState(null);
 
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -75,6 +79,53 @@ const FinancialAddForm = () => {
     setFileCountList(countArr);
   };
 
+  const [flag, setFlag] = useState(false);
+  const imgLink = 'http://localhost:3000/img';
+
+  const customUploadAdapter = (loader) => {
+    // (2)
+    return {
+      upload() {
+        return new Promise((resolve, reject) => {
+          const token = getCookieToken();
+          const config = {
+            headers: {
+              Authorization: token,
+            },
+          };
+          const data = new FormData();
+          loader.file.then((file) => {
+            data.append('name', file.name);
+            data.append('file', file);
+
+            console.log('name : ' + file.name);
+            axios
+              .post(serverUrl + '/api/file/', data, config)
+              .then((res) => {
+                if (!flag) {
+                  console.log('filename : ' + res.data.filename);
+                  setFlag(true);
+                  // setImage(res.data.filename);
+                  setImage('5bd0e843-7c73-4ec2-96ac-468bde7e6462.png');
+                }
+                resolve({
+                  // default: `${imgLink}/${res.data.filename}`,
+                  default: `${imgLink}/download/wordmark.png`,
+                });
+              })
+              .catch((err) => reject(err));
+          });
+        });
+      },
+    };
+  };
+
+  function uploadPlugin(editor) {
+    editor.plugins.get('FileRepository').createUploadAdapter = (loader) => {
+      return customUploadAdapter(loader);
+    };
+  }
+
   return (
     <div className="a-content">
       <h2>재무등록</h2>
@@ -101,6 +152,9 @@ const FinancialAddForm = () => {
           <div className="ed-area">
             <CKEditor
               editor={ClassicEditor}
+              config={{
+                extraPlugins: [uploadPlugin],
+              }}
               data=""
               onChange={(event, editor) => {
                 const data = editor.getData();
