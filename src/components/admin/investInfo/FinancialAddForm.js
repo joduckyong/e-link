35421a04c7c6df4cd148c83a1e6dfe1765bd9edd
joduckyong <dyddl7609@gmail.com>
2023-01-13 14:00:16 +0900

    에디터 수정

--- src/components/admin/investInfo/FinancialAddForm.js
+++ src/components/admin/investInfo/FinancialAddForm.js
@@ -1,12 +1,12 @@
-import React, { useState, useRef, useCallback } from 'react';
+import React, { useState, useRef, useCallback, useMemo } from 'react';
 import axios from 'axios';
 import { NavLink, useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { insertBoard } from 'store/boardReducer';
 import { serverUrl } from 'store/serverUrl';
 import { getCookieToken } from 'storage/Cookie';
-import { CKEditor } from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 const AddFileBox = ({ fileName, filesRef, onUploadFile, onDeleteFile, fileCountList }) => {
   return (
@@ -42,6 +42,7 @@ const FinancialAddForm = (props) => {
   const dispatch = useDispatch();
 
   const filesRef = useRef([]);
+  const quillRef = useRef();
 
   //등록
   const onCreate = async (e) => {
@@ -95,43 +96,124 @@ const FinancialAddForm = (props) => {
     setFileCountList(countArr);
   };
 
-  const [flag, setFlag] = useState(false);
-  const customUploadAdapter = (loader) => {
-    return {
-      upload() {
-        return new Promise((resolve, reject) => {
-          const token = getCookieToken();
-          const config = {
-            headers: {
-              Authorization: token,
-            },
-          };
-          const data = new FormData();
-          loader.file.then((file) => {
-            // data.append('name', file.name);
-            data.append('file', file);
-            axios
-              .post(serverUrl + '/api/file/', data, config)
-              .then((res) => {
-                if (!flag) {
-                  setFlag(true);
-                }
-                resolve({
-                  default: `${serverUrl}/api/file/img/${res.data}`,
-                });
-              })
-              .catch((err) => reject(err));
-          });
-        });
-      },
-    };
+  const imageHandler = () => {
+    console.log('에디터에서 이미지 버튼을 클릭하면 이 핸들러가 시작됩니다!');
+
+    // 1. 이미지를 저장할 input type=file DOM을 만든다.
+    const input = document.createElement('input');
+    // 속성 써주기
+    input.setAttribute('type', 'file');
+    input.setAttribute('accept', 'image/*');
+    document.body.appendChild(input);
+    input.click(); // 에디터 이미지버튼을 클릭하면 이 input이 클릭된다.
+    // input이 클릭되면 파일 선택창이 나타난다.
+
+    // input에 변화가 생긴다면 = 이미지를 선택
+    input.addEventListener('change', async () => {
+      console.log('온체인지');
+      const file = input.files[0];
+      // multer에 맞는 형식으로 데이터 만들어준다.
+      const formData = new FormData();
+      formData.append('file', file); // formData는 키-밸류 구조
+      // 백엔드 multer라우터에 이미지를 보낸다.
+      try {
+        const token = getCookieToken();
+        const config = {
+          headers: {
+            Authorization: token,
+          },
+        };
+        const result = await axios.post(serverUrl + '/api/file/', formData, config);
+        console.log('성공 시, 백엔드가 보내주는 데이터', result.data);
+        const IMG_URL = `${serverUrl}/api/file/img/${result.data}`;
+        console.log('IMG_URL : ' + IMG_URL);
+        // 이 URL을 img 태그의 src에 넣은 요소를 현재 에디터의 커서에 넣어주면 에디터 내에서 이미지가 나타난다
+        // src가 base64가 아닌 짧은 URL이기 때문에 데이터베이스에 에디터의 전체 글 내용을 저장할 수있게된다
+        // 이미지는 꼭 로컬 백엔드 uploads 폴더가 아닌 다른 곳에 저장해 URL로 사용하면된다.
+
+        // 이미지 태그를 에디터에 써주기 - 여러 방법이 있다.
+        const editor = quillRef.current.getEditor(); // 에디터 객체 가져오기
+        // 1. 에디터 root의 innerHTML을 수정해주기
+        // editor의 root는 에디터 컨텐츠들이 담겨있다. 거기에 img태그를 추가해준다.
+        // 이미지를 업로드하면 -> 멀터에서 이미지 경로 URL을 받아와 -> 이미지 요소로 만들어 에디터 안에 넣어준다.
+        // editor.root.innerHTML =
+        //   editor.root.innerHTML + `<img src=${IMG_URL} /><br/>`; // 현재 있는 내용들 뒤에 써줘야한다.
+
+        // 2. 현재 에디터 커서 위치값을 가져온다
+        const range = editor.getSelection();
+        // 가져온 위치에 이미지를 삽입한다
+        editor.insertEmbed(range.index, 'image', IMG_URL);
+      } catch (error) {
+        console.log('실패했어요ㅠ');
+      }
+    });
   };
 
-  function uploadPlugin(editor) {
-    editor.plugins.get('FileRepository').createUploadAdapter = (loader) => {
-      return customUploadAdapter(loader);
-    };
+  function onEditorChange(value) {
+    setBoardContents(value);
   }
+
+  const modules = useMemo(() => {
+    return {
+      toolbar: {
+        container: [
+          [{ header: [1, 2, 3, 4, 5, 6, false] }],
+          [{ font: [] }],
+          [{ align: [] }],
+          ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+          [{ list: 'ordered' }, { list: 'bullet' }, 'link'],
+          [
+            {
+              color: [
+                '#000000',
+                '#e60000',
+                '#ff9900',
+                '#ffff00',
+                '#008a00',
+                '#0066cc',
+                '#9933ff',
+                '#ffffff',
+                '#facccc',
+                '#ffebcc',
+                '#ffffcc',
+                '#cce8cc',
+                '#cce0f5',
+                '#ebd6ff',
+                '#bbbbbb',
+                '#f06666',
+                '#ffc266',
+                '#ffff66',
+                '#66b966',
+                '#66a3e0',
+                '#c285ff',
+                '#888888',
+                '#a10000',
+                '#b26b00',
+                '#b2b200',
+                '#006100',
+                '#0047b2',
+                '#6b24b2',
+                '#444444',
+                '#5c0000',
+                '#663d00',
+                '#666600',
+                '#003700',
+                '#002966',
+                '#3d1466',
+                'custom-color',
+              ],
+            },
+            { background: [] },
+          ],
+          ['image'],
+          ['clean'],
+        ],
+        handlers: {
+          image: imageHandler,
+        },
+      },
+    };
+  }, []);
 
   return (
     <div className="a-content">
@@ -157,41 +239,7 @@ const FinancialAddForm = (props) => {
             />
           </div>
           <div className="ed-area">
-            <CKEditor
-              editor={ClassicEditor}
-              config={{
-                language: 'ko',
-                toolbar: [
-                  'heading',
-                  '|',
-                  'bold',
-                  'italic',
-                  'link',
-                  // 'bulletedList',
-                  // 'numberedList',
-                  'blockQuote',
-                  '|',
-                  'imageTextAlternative',
-                  'imageUpload',
-                  'imageStyle:full',
-                  'imageStyle:side',
-                  '|',
-                  'insertTable',
-                  'tableColumn',
-                  'tableRow',
-                  'mergeTableCells',
-                  '|',
-                  'undo',
-                  'redo',
-                ],
-                extraPlugins: [uploadPlugin],
-              }}
-              data=""
-              onChange={(event, editor) => {
-                const data = editor.getData();
-                setBoardContents(data);
-              }}
-            />
+            <ReactQuill ref={quillRef} value={boardContents} onChange={setBoardContents} modules={modules} />
           </div>
 
           <div className="ed-file">
