f334163b2d741598e048ce97e07a493f8e49dca9
Kang <khighbig@gmail.com>
2023-04-07 17:53:55 +0900

    연혁 작업중

--- src/components/admin/company/OutlineForm.js
+++ src/components/admin/company/OutlineForm.js
@@ -1,13 +1,55 @@
 import React, { useState } from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
 import Pagination from 'react-js-pagination';
+
+const AddYearBox = ({ yearCountList }) => {
+  return (
+    <>
+      {yearCountList.map((index, idx) => (
+        <li key={index}>
+          <div class="year-hd">
+            <button class="delete">
+              <img src="/img/admin/year-close.png" alt="삭제" />
+            </button>
+            <input type="text" placeholder="yyyy" maxlength="4" />
+          </div>
+          <ul class="year-bd">
+            <li class="active">
+              <div class="input-wp">
+                <input type="text" placeholder="mm" maxlength="2" />
+                <input type="text" placeholder="dd" maxlength="2" />
+                <input type="text" placeholder="내용을 입력해주세요." />
+              </div>
+              <div class="btn-wp">
+                <button class="gray-btn">등록</button>
+                <button class="red-btn">삭제</button>
+              </div>
+            </li>
+          </ul>
+          <button class="add-btn">+ 내용추가</button>
+        </li>
+      ))}
+    </>
+  );
+};
 
 const OutlineForm = () => {
   const totalCount = 0;
   const [page, setPage] = useState(1);
+  const [yearCountList, setYearCountList] = useState([0]);
 
   const pageClick = (page) => {
     setPage(page);
   };
+
+  const onAddYearBox = () => {
+    let countArr = [...yearCountList];
+    let count = countArr.slice(-1)[0] ? countArr.slice(-1)[0] : 0;
+    count += 1;
+    countArr.push(count);
+    setYearCountList(countArr);
+  };
+
   return (
     <div class="a-content">
       <h2>
@@ -15,80 +57,15 @@ const OutlineForm = () => {
       </h2>
       <div class="ban-list p0">
         <div class="btn-area position">
-          <button class="btn btn-blue btn-120">연도생성</button>
+          <NavLink to="" className="btn-add" onClick={onAddYearBox}>
+            <button class="btn btn-blue btn-120">연도생성</button>
+          </NavLink>
         </div>
         <div class="table-wrap">
           <ul class="year-wp">
-            {/* <!-- [s] 수정 눌렀을 때 --> */}
-            <li>
-              <div class="year-hd">
-                <button class="delete">
-                  <img src="/img/admin/year-close.png" alt="삭제" />
-                </button>
-                <input type="text" placeholder="yyyy" maxlength="4" />
-              </div>
-              <ul class="year-bd">
-                <li class="active">
-                  {/*  <div class="arrow-wp">
-          <button class="arrow-btn"><img src="/img/admin/year-arrow.png" alt="위로"/></button>
-          <button class="arrow-btn"><img src="/img/admin/year-arrow.png" alt="아래로"/></button>
-        </div>  */}
-                  <div class="input-wp">
-                    <input type="text" placeholder="mm" maxlength="2" />
-                    <input type="text" placeholder="dd" maxlength="2" />
-                    <input type="text" placeholder="내용을 입력해주세요." />
-                  </div>
-                  <div class="btn-wp">
-                    <button class="gray-btn">등록</button>
-                    <button class="red-btn">삭제</button>
-                  </div>
-                </li>
-              </ul>
-              <button class="add-btn">+ 내용추가</button>
-            </li>
-            {/* <!-- [e] 수정 눌렀을 때 --> */}
-
-            <li>
-              <div class="year-hd">
-                <button class="delete">
-                  <img src="/img/admin/year-close.png" alt="삭제" />
-                </button>
-                <input type="text" value="2023" disabled />
-              </div>
-              <ul class="year-bd">
-                <li>
-                  {/*  <div class="arrow-wp">
-          <button class="arrow-btn"><img src="/img/admin/year-arrow.png" alt="위로"/></button>
-          <button class="arrow-btn"><img src="/img/admin/year-arrow.png" alt="아래로"/></button>
-        </div>  */}
-                  <div class="input-wp">
-                    <input type="text" value="07" disabled />
-                    <input type="text" value="08" disabled />
-                    <input type="text" value="전기공사업 등록" disabled />
-                  </div>
-                  <div class="btn-wp">
-                    <button class="white-btn">수정</button>
-                    <button class="red-btn">삭제</button>
-                  </div>
-                </li>
-                <li>
-                  {/*  <div class="arrow-wp">
-          <button class="arrow-btn"><img src="/img/admin/year-arrow.png" alt="위로"/></button>
-          <button class="arrow-btn"><img src="/img/admin/year-arrow.png" alt="아래로"/></button>
-        </div>  */}
-                  <div class="input-wp">
-                    <input type="text" placeholder="mm" maxlength="2" />
-                    <input type="text" placeholder="dd" maxlength="2" />
-                    <input type="text" placeholder="내용을 입력해주세요." />
-                  </div>
-                  <div class="btn-wp">
-                    <button class="gray-btn">등록</button>
-                    <button class="red-btn">삭제</button>
-                  </div>
-                </li>
-              </ul>
-              <button class="add-btn">+ 내용추가</button>
-            </li>
+            <AddYearBox
+              yearCountList={yearCountList}
+            />
           </ul>
         </div>
         <div className="paging">
