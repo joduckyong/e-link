d9f8dba44e19d50796568bf0156f867f7101bdb7
joduckyong <dyddl7609@gmail.com>
2023-05-08 13:08:34 +0900

    개발중

--- src/components/client/ev/login/FindPwForm.js
+++ src/components/client/ev/login/FindPwForm.js
@@ -1,15 +1,126 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { encrypt } from '../../../../api/crypto';
 
-const FindPwForm = () => {
+const FindIdForm = () => {
+  const [telno, setTelno] = useState('');
+  const [ci, setCi] = useState('');
+  const [pswd, setPswd] = useState('');
+  const [rePswd, setRepswd] = useState('');
+
+  const [phoneCk, setPhoneCk] = useState(false);
+
+  const [style, setStyle] = useState({ display: 'block' });
+  const [style2, setStyle2] = useState({ display: 'none' });
+
+  // 핸드폰 등록
+  const phoneChange = (e) => {
+    const { value } = e.target;
+    const onlyNumber = value.replace(/[^0-9]/g, '');
+    setTelno(onlyNumber);
+  };
+
+  // 휴대폰인증
+  const phonePopup = (e) => {
+    e.preventDefault();
+    if (telno.length !== 11) {
+      alert('휴대폰 번호를 입력해주세요.');
+      return;
+    }
+    setPhoneCk(true);
+    window.open(process.env.REACT_APP_API_URL + '/api/phone/popup2?type=V', 'width=0,height=0,location=no,status=no,scrollbars=yes', '_blank');
+  };
+
+  const signupUserInfo = (e) => {
+    e.preventDefault();
+
+    if (telno.length !== 11) {
+      alert('휴대폰 번호를 입력해주세요.');
+      return;
+    }
+
+    if (!phoneCk) {
+      alert('휴대폰 본인인증을 해주세요.');
+      return;
+    }
+
+    fetch(process.env.REACT_APP_API_URL + '/api/phone/phoneInfo/' + telno, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+        Accept: 'application/json',
+      },
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((res) => {
+        console.log('res.data : ' + res.data);
+
+        if (res.data !== undefined) {
+          const dataCi = JSON.stringify(res.data.ci).replace(/"/g, '');
+          setCi(dataCi);
+
+          setStyle({ display: 'none' });
+          setStyle2({ display: 'block' });
+        } else {
+          alert('휴대폰 본인인증을 해주세요.');
+          return;
+        }
+      });
+  };
+
+  const passChange = () => {
+    if (pswd !== rePswd) {
+      alert('비밀 번호가 일치 하시 않습니다.');
+      return;
+    }
+
+    const data = {
+      url: '/auth/resetPassword',
+      ci: ci,
+      pswd: encrypt(pswd),
+    };
+
+    console.log('data : ' + JSON.stringify(data));
+
+    fetch(process.env.REACT_APP_API_URL + '/api/ev/auth/resetPassword', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((res) => {
+        if (res.data !== undefined) {
+          const result = JSON.stringify(res.data.success).replace(/"/g, '');
+          if (result) {
+            alert('비밀번호가 변경 되었습니다.');
+          }
+        }
+      });
+  };
+
   return (
     <>
       <section className="ev-sub-sect">
-        <form className="findpw-wp">
+        <form className="findpw-wp" style={style}>
           <h1>비밀번호 찾기</h1>
+
           <div className="gray-box">
-            <h3 className="mini-ttl">아이디</h3>
-            <input type="text" placeholder="아이디를 입력해주세요." />
-            <button className="orange-btn">휴대폰 본인인증</button>
+            <h3 className="mini-ttl">휴대폰</h3>
+            <input type="text" placeholder="휴대폰번호를 입력해주세요." name="telno" value={telno} onChange={phoneChange} maxlength={11} />
+            <button className="orange-btn" onClick={phonePopup}>
+              휴대폰 본인인증
+            </button>
+            <button className="orange-btn" onClick={signupUserInfo}>
+              비밀번호 변경
+            </button>
+
             <div className="notice">
               <img src="/img/ev/ev_notice_ico.png" alt="!" />
               소셜 간편회원은 <br className="mo-br" />
@@ -18,9 +129,34 @@ const FindPwForm = () => {
             </div>
           </div>
         </form>
+
+        <form className="findid-wp" style={style2}>
+          <h1>비밀번호 변경</h1>
+
+          <div className="gray-box">
+            <h3 className="mini-ttl">비밀번호</h3>
+            <input type="password" placeholder="비밀번호를 입력해주세요." onChange={(e) => setPswd(e.target.value)} value={pswd} maxlength={15} />
+            <h3 className="mini-ttl">비밀번호 확인</h3>
+            <input
+              type="password"
+              placeholder="비밀번호를 다시 한 번 입력해주세요. "
+              onChange={(e) => setRepswd(e.target.value)}
+              value={rePswd}
+              maxlength={15}
+            />
+            <h3 className="mini-ttl"></h3>
+            <button className="orange-btn" onClick={passChange}>
+              비밀번호 변경
+            </button>
+            <h3 className="mini-ttl"></h3>
+            <NavLink to="/ev/login" className="orange-btn">
+              로그인
+            </NavLink>
+          </div>
+        </form>
       </section>
     </>
   );
 };
 
-export default FindPwForm;
+export default FindIdForm;
