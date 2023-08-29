8722ff97e99828c818b64cb7785b347075d44585
Kang <khighbig@gmail.com>
2023-08-29 17:17:48 +0900

    작업중

--- src/components/client/ev/BreakdownAddForm.js
+++ src/components/client/ev/BreakdownAddForm.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectEv } from 'store/EvReducer';
 
 const BreakdownAddForm = () => {
+  const dispatch = useDispatch();
+  const rechgList = useSelector((state) => state.EvReducer.data);
+  const [selectItem, setSelectItem] = useState('');
+
+  // useEffect(() => {
+  //   const url = '/api/m-service-mobile/rechgst/getELinkRechgsts';
+  //   const newList = { url: url };
+  //   dispatch(selectEv(newList));
+  // }, [dispatch]);
+
   return (
     <>
       <section className="ev-sub-sect">
@@ -14,7 +26,11 @@ const BreakdownAddForm = () => {
               </label> */}
             </div>
           </div>
-
+          <select name="cate" id="cate" onChange={(e) => setSelectItem(e.target.value)}>
+              {/* <option value="Q010" selected={selectItem === 'Q010' && true}>사용법</option>
+              <option value="Q020" selected={selectItem === 'Q020' && true}>결제/환불</option>
+              <option value="Q030" selected={selectItem === 'Q030' && true}>기타</option> */}
+          </select>
           <input type="text" placeholder="제목을 입력해주세요." />
           <textarea name="" id="" cols="30" rows="10" placeholder="문의 하실 내용을 입력해주세요."></textarea>
 
